321dbf0b0c62c2ab8047c7d6292530258e7fadd1
kashishtoronto69 <sachdkas@sheridancollge.ca>
2019-05-15 11:30:22 -0400

    StudentDemo

--- src/softwareprogram/SoftwareProgram.java
+++ src/softwareprogram/SoftwareProgram.java
@@ -15,7 +15,7 @@ public class SoftwareProgram {
     int sid;
     public static void main(String[] args) {
         Student obj1 = new Student();
-        obj1.setName("kashish");
+        obj1.setName("");
         System.out.println(obj1);
     }
     
