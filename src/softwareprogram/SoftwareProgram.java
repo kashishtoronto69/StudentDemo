90b655d68a85e62f9c383f5f0b596aac2431ebea
kashishtoronto69 <sachdkas@sheridancollge.ca>
2019-05-15 11:02:52 -0400

    StudentDemo

---
+++ src/softwareprogram/SoftwareProgram.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package softwareprogram;
+
+/**
+ *
+ * @author kashi
+ */
+public class SoftwareProgram {
+    String name;
+    int age;
+    int sid;
+    public static void main(String[] args) {
+        Student obj1 = new Student();
+        obj1.setName("kashish");
+        System.out.println(obj1);
+    }
+    
+}
